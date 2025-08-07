689feb782265bdf8b43b50e988b08eedf720fc0a
iseki <admin@iseki.space>
2025-08-08 06:00:13 +0900

    build: add toolchain config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
 rootProject.name = "purlkt"
 
+toolchainManagement {
+    jvm { 
+        javaRepositories {
+            repository("foojay") { 
+                resolverClass = org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java
+            }
+        }
+    }
+}
