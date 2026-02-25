1d393ac10b62b5977550f115700aad1b3a6b539b
iseki <admin@iseki.space>
2026-02-25 16:47:23 +0800

    migrate maven central publishing to vanniktech plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
 @file:OptIn(ExperimentalWasmDsl::class, ExperimentalAbiValidation::class)
 
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 import java.net.URI
-import java.util.*
 
 plugins {
     kotlin("multiplatform") version "2.3.0"
     kotlin("plugin.serialization") version "2.3.0"
     id("org.jetbrains.dokka") version "2.0.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.35.0"
     signing
 }
 
@@ -83,80 +84,44 @@ tasks.named("jvmTest") {
 
 signing {
     useGpgCmd()
-    sign(publishing.publications)
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "Central"
-            afterEvaluate {
-                url = if (version.toString().endsWith("SNAPSHOT")) {
-                    // uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-                    uri("https://central.sonatype.com/repository/maven-snapshots/")
-                } else {
-                    // uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
-                    // uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-                    uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
-                }
-            }
-            credentials {
-                username = properties["ossrhUsername"]?.toString() ?: System.getenv("OSSRH_USERNAME")
-                password = properties["ossrhPassword"]?.toString() ?: System.getenv("OSSRH_PASSWORD")
+mavenPublishing {
+    configure(KotlinMultiplatform(JavadocJar.Empty()))
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(groupId = group.toString(), artifactId = rootProject.name, version = version.toString())
+    pom {
+        val projectUrl = "https://github.com/iseki0/PurlKt"
+        name = "PurlKt"
+        description = "A library for purl parsing and building, in Kotlin multiplatform"
+        url = projectUrl
+        licenses {
+            license {
+                name = "Apache-2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0"
             }
         }
-        if (!System.getenv("GITHUB_TOKEN").isNullOrBlank()) {
-            maven {
-                name = "GitHubPackages"
-                url = URI.create("https://maven.pkg.github.com/iseki0/PurlKt")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")!!
-                    password = System.getenv("GITHUB_TOKEN")!!
-                }
+        developers {
+            developer {
+                id = "iseki0"
+                name = "iseki zero"
+                email = "iseki@iseki.space"
             }
         }
-    }
-    publications {
-        withType<MavenPublication> {
-            val pubName = name.replaceFirstChar { it.titlecase(Locale.getDefault()) }
-            val emptyJavadocJar by tasks.register<Jar>("emptyJavadocJar$pubName") {
-                archiveClassifier = "javadoc"
-                archiveBaseName = artifactId
-            }
-            artifact(emptyJavadocJar)
-            pom {
-                name = "PurlKt-${project.name}"
-                val projectUrl = "https://github.com/iseki0/PurlKt"
-                description = "A library for purl parsing and building, in Kotlin multiplatform"
-                url = projectUrl
-                licenses {
-                    license {
-                        name = "Apache-2.0"
-                        url = "https://www.apache.org/licenses/LICENSE-2.0"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "iseki0"
-                        name = "iseki zero"
-                        email = "iseki@iseki.space"
-                    }
-                }
-                inceptionYear = "2025"
-                scm {
-                    connection = "scm:git:$projectUrl.git"
-                    developerConnection = "scm:git:$projectUrl.git"
-                    url = projectUrl
-                }
-                issueManagement {
-                    system = "GitHub"
-                    url = "$projectUrl/issues"
-                }
-                ciManagement {
-                    system = "GitHub"
-                    url = "$projectUrl/actions"
-                }
-            }
+        inceptionYear = "2025"
+        scm {
+            connection = "scm:git:$projectUrl.git"
+            developerConnection = "scm:git:$projectUrl.git"
+            url = projectUrl
+        }
+        issueManagement {
+            system = "GitHub"
+            url = "$projectUrl/issues"
+        }
+        ciManagement {
+            system = "GitHub"
+            url = "$projectUrl/actions"
         }
     }
 }
